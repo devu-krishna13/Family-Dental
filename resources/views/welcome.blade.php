7f89f5bcc898ebc7aedd538958a1bcb78d2d7018
Devu Krishnan P <devukrishnanp@gmail.com>
2026-08-14 11:17:00 +0530

    pull changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -563,6 +563,19 @@
                 </div>
             </div>
         </section>
+
+        <!-- Location Map Section -->
+        <section class="map-section bg-alt" id="location">
+            <div class="container">
+                <div class="map-header">
+                    <h2>Visit Our Clinic</h2>
+                    <p>Conveniently located in Kanjippuzha to serve you better</p>
+                </div>
+                <div class="map-container">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.3697445803323!2d76.56293889999999!3d9.121036700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b061b264a857cef%3A0x1008df66628f1f3a!2sFamily%20Dental%20Care%2C%20Kanjippuzha!5e0!3m2!1sen!2sin!4v1786685539718!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                </div>
+            </div>
+        </section>
     </main>
 
     <!-- Premium Footer -->
